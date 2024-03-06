1c1de3547c4e027e59d76b8d951b0348256a8bc5
lily <yeahg.dev@gmail.com>
2024-03-06 10:35:25 +0900

    add project

---
+++ ItemsRepeaterSample/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Windows.UI.Core;
+
+namespace ItemsRepeaterSample;
+
+public sealed partial class MainPage : Page
+{
+    private ObservableCollection<string> items = new ObservableCollection<string>{"Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Kiwi", "Lemon", "Mango", "Nectarine"};
+    private int newItemIndex = 0;
+
+    public MainPage()
+    {
+        this.InitializeComponent();
+        this.DataContext = items;
+        this.items.CollectionChanged += OnCollectinChanged;
+    }
+
+    private async void OnCollectinChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+        {
+            this.ScrollViewer.ChangeView(null, int.MaxValue, null, true);
+        });
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        items.Add("New Item " + newItemIndex.ToString());
+        newItemIndex++;
+    }
+}
